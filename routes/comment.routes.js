fa9c688a763d9330737ce7d9e0f9dc584099f333
ibirossi <ibi.homaidan@gmail.com>
2020-11-06 18:00:39 +0100

    comment routes completed, but may need more tests on postman.  Project routes updated due to errors found in postman.  lib file changed to .json to fix error (not iterable).  Need to test and work out how to do edit project route.  It will work if you place the _id in the :id part of the route.  DOnt know why the logic used for create project doesnt work here

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -3,9 +3,76 @@ const { isLoggedIn } = require("../helpers/auth-helper");
 const router = express.Router();
 
 let CommentModel = require('../models/comments.model')
+let ProjectModel = require('../models/project.model')
+
+
 
 router.get('/comment/:projectId', isLoggedIn, (req, res) => {
   CommentModel.findById(req.params.projectId)
+  .then((response) => {
+    res.status(200).json(response);
+  })
+  .catch((err) => {
+    res.status(500).json({
+      error: 'Something went wrong with view comment',
+      message: err
+    })
+  })
 })
 
 
+router.post('/comment/:projectId', isLoggedIn, (req,res) => {
+  let projectId = req.params.projectId
+  const {userRefId, commentBody, commentFlag, creatorCheck, projectVersion} 
+  = req.body
+
+  CommentModel.create({userRefId, commentBody, commentFlag, creatorCheck, projectVersion})
+  .then((response) => {
+    let commentId = response._id
+    res.status(200).json(response)
+    ProjectModel.findByIdAndUpdate(projectId, {$push: {comments: commentId}})
+    .then(() => {
+      res.status(200).json({
+        message: 'Comment added'
+      })
+    })
+  })
+  .catch((err) => {
+    res.status(500).json({
+      error: 'Something went wrong with create comment',
+      message: err
+    })
+  })
+})
+
+router.delete('/comment/:commentId', isLoggedIn, (req, res) => {
+  let commentId = req.params.commentId
+  let loggedInUserId = req.session.loggedInUser._id
+
+  CommentModel.findById(commentId)
+  .then((response) => {
+    if (response.userRefId == loggedInUserId){
+      CommentModel.findByIdAndDelete(commentId)
+      .then(() => {
+        res.status(200).json({
+          message: 'Comment deleted successfully'
+        })
+      
+      })
+      .catch((err) => {
+        res.status(500).json({
+          error: 'You cannot delete this comment',
+          message: err
+        })
+      })
+    }
+  })
+
+
+
+})
+
+
+
+
+module.exports = router
--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -6,7 +6,7 @@ let UserModel = require("../models/user.model");
 
 
 //view profile
-router.get('/profile/:id', isLoggedIn, (req, res) => {
+router.get('/profile/:id', (req, res) => {
   UserModel.findById(req.params.id)
     .then((response) => {
       res.status(200).json(response);
@@ -21,11 +21,12 @@ router.get('/profile/:id', isLoggedIn, (req, res) => {
 
 
 //edit your profile
-router.post('/profile/:id', isLoggedIn, (req, res) => {
+router.post('/profile/:id',/*isLoggedIn,*/ (req, res) => {
   let id = req.params.id
   const {aboutMe, mySkills, userImage} = req.body; 
-  UserModel.findByIdAndUpdate(id, {set: {aboutMe: aboutMe, mySkills: mySkills, userImage: userImage}})
+  UserModel.findByIdAndUpdate(id, {$set: {aboutMe: aboutMe, mySkills: mySkills, userImage: userImage}})
   .then((response) => {
+    
     res.status(200).json(response)
   })
   .catch((err) => {
